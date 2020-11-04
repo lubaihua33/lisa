57fc44ab9642ba9473e1e42c1294f6d09acb341b
Baihua Lu <balu@microsoft.com>
2020-11-04 09:57:17 +0800

    Fix for the new TestPassCache table

--- Utilities/Run-AzureFleetSmokeTest.ps1
+++ Utilities/Run-AzureFleetSmokeTest.ps1
@@ -15,100 +15,105 @@ Documentation
     Purpose/Change:
 
 .EXAMPLE
-    Run-AzureFleetSmokeTest.ps1 -TestLocation "eastus"
+    Run-AzureFleetSmokeTest.ps1 -Location "eastus"
 #>
 ###############################################################################################
 Param
 (
     [string] $AzureSecretsFile,
-    [string] $TestLocation
+    [string] $Location
 )
 
-# Read secrets file and terminate if not present.
-Write-Host "Info: Check the Azure Secrets File..."
-if (![String]::IsNullOrEmpty($AzureSecretsFile) -and (Test-Path -Path $AzureSecretsFile)) {
-    $Secrets = ([xml](Get-Content $AzureSecretsFile))
-} else {
-    Write-Host "Error: Please provide value for -AzureSecretsFile"
-    exit 1
-}
-if ((![String]::IsNullOrEmpty($Secrets)) -and (![String]::IsNullOrEmpty($Secrets.secrets))) {
-    Set-Variable -Name XmlSecrets -Value $Secrets -Scope Global -Force
-} else {
-    Write-Host "Secrets file not found. Exiting."
-    exit 1
-}
-Write-Host "Info: Check the Azure Secrets File OK"
+$StatusRunning = "Running"
 
-Function Run-SmokeTestbyLISAv3($ARMImage, $TestLocation)
+Function Invoke-SmokeTest($ARMImage, $Location)
 {
-    Write-Host "Info: Run smoke test for $ARMImage in $TestLocation"
+    Write-Host "Info: Run smoke test for $ARMImage in $Location"
 
     $gPublisher = $ARMImage.split(' ')[0]
     $gOffer = $ARMImage.split(' ')[1]
     $gSku = $ARMImage.split(' ')[2]
     $gVersion = $ARMImage.split(' ')[3]
 
-    # Temp code check if have plan
-    Write-Host "Info: Get-AzVMImage and check if has plan"
-    if ($gVersion -ne "latest") {
-        $used_image = Get-AzVMImage -Location $TestLocation -PublisherName $gPublisher -Offer $gOffer -Skus $gSku -Version $gVersion
-    } else {
-        $used_image = Get-AzVMImage -Location $TestLocation -PublisherName $gPublisher -Offer $gOffer -Skus $gSku
-        $used_image = Get-AzVMImage -Location $TestLocation -PublisherName $gPublisher -Offer $gOffer -Skus $gSku -Version $used_image[-1].Version
-    }
-    if ($used_image.PurchasePlan) {
-        Write-Host "Info: The Image $ARMImage has plan. Skip testing the image."
-    } else {
-        Write-Host "Info: The Image $ARMImage has no plan. Continue testing the image."
-        Set-Location -Path ".\lisa"
-        Write-Host "Info: poetry run python lisa/main.py -r ..\runbook\smoke.yml -v gPublisher:${gPublisher} -v gOffer:${gOffer} -v gSku:${gSku} -v gVersion:${gVersion} -v location:${TestLocation} -v adminPrivateKeyFile:$($env:LISA_PRI_SECUREFILEPATH)"
-        poetry run python lisa/main.py -r ..\runbook\smoke.yml -v gPublisher:${gPublisher} -v gOffer:${gOffer} -v gSku:${gSku} -v gVersion:${gVersion} -v location:${TestLocation} -v adminPrivateKeyFile:"$($env:LISA_PRI_SECUREFILEPATH)"
-        Set-Location -Path "..\"
-    }
+    $logInfo = "Info: .\lisa -r runbook\smoke.yml " +
+    "-v gPublisher:${gPublisher} -v gOffer:${gOffer} -v gSku:${gSku} -v gVersion:${gVersion}" + 
+    "-v location:${Location} -v adminPrivateKeyFile:$env:LISA_PRI_SECUREFILEPATH"
+    Write-Host $logInfo
+
+    .\lisa -r runbook\smoke.yml `
+    -v gPublisher:${gPublisher} -v gOffer:${gOffer} -v gSku:${gSku} -v gVersion:${gVersion} `
+    -v location:${Location} -v adminPrivateKeyFile:$env:LISA_PRI_SECUREFILEPATH
 }
 
-$BuildNumber = $env:BUILD_BUILDNUMBER
-$sql = "select ARMImage from AzureFleetSmokeTestDistroList where BuildID like '$BuildNumber' and RunStatus like 'START'"
+# Read secrets file and terminate if not present.
+Write-Host "Info: Check the Azure Secrets File..."
+if (![String]::IsNullOrEmpty($AzureSecretsFile) -and (Test-Path -Path $AzureSecretsFile)) {
+    $content = Get-Content -Path $AzureSecretsFile
+    foreach ($line in $content) {
+        if ($line.split(':')[0] -eq 'dbUserName') {
+            $dbuser = $line.split(':')[1].trim()
+        }
+        if ($line.split(':')[0] -eq 'dbPassword') {
+            $dbpassword = $line.split(':')[1].trim()
+        }
+    }
+} else {
+    Write-Host "Error: Please provide value for -AzureSecretsFile"
+    exit 1
+}
 
-$server = $XmlSecrets.secrets.DatabaseServer
-$dbuser = $XmlSecrets.secrets.DatabaseUser
-$dbpassword = $XmlSecrets.secrets.DatabasePassword
-$database = $XmlSecrets.secrets.DatabaseName
+# Read variable_database file
+$variableFile = ".\runbook\variable_database.yml"
+if (Test-Path -Path $variableFile) {
+    $content = Get-Content -Path $variableFile
+    foreach ($line in $content) {
+        if ($line.split(':')[0] -eq 'dbServerName') {
+            $server = $line.split(':')[1].trim()
+        }
+        if ($line.split(':')[0] -eq 'dbName') {
+            $database = $line.split(':')[1].trim()
+        }
+    }
+} else {
+    Write-Host "Error: No .\runbook\variable_database.yml"
+    exit 1
+}
+Write-Host "Info: Check the secrets File and variable_database file OK"
 
-if ($server -and $dbuser -and $dbpassword -and $database) {
-    Write-Host "Info: SQLQuery:  $sql"
-    $connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;MultipleActiveResultSets=True;"
+if (!$server -or !$dbuser -or !$dbpassword -or !$database) {
+    Write-Host "Error: Database details are not provided."
+    exit 1
+}
+
+$BuildId = $env:BUILD_BUILDNUMBER
+$connectionString = "Server=$server;uid=$dbuser; pwd=$dbpassword;Database=$database;Encrypt=yes;" +
+                    "TrustServerCertificate=no;Connection Timeout=30;MultipleActiveResultSets=True;"
+
+try {
     $connection = New-Object System.Data.SqlClient.SqlConnection
     $connection.ConnectionString = $connectionString
     $connection.Open()
-    $retry = 0
-    While ($retry -lt 3) {
-        $dataset = new-object "System.Data.Dataset"
-        $dataadapter = new-object "System.Data.SqlClient.SqlDataAdapter" ($sql, $connection)
-        $recordcount = $dataadapter.Fill($dataset)
-        foreach ($row in $dataset.Tables.rows) {
-            $image = $row.ARMImage
-            Run-SmokeTestbyLISAv3 -ARMImage $image -TestLocation $TestLocation
-            $sql = "Update AzureFleetSmokeTestDistroList Set RunStatus='DONE' where ARMImage='$image'"
-            $command = $connection.CreateCommand()
-            $command.CommandText = $sql
-            $null = $command.ExecuteNonQuery()
-            $command.Dispose()
-            Write-Host "Update AzureFleetSmokeTestDistroList RunStatus to 'DONE' where ARMImage is $image"
-        }
-        if ($recordcount -eq 0) {
-            Start-Sleep -Seconds 5
-            $retry += 1
-        }
+    $sql = "
+    select ARMImage from TestPassCache 
+    where Context='$BuildId' and Status='$StatusRunning'
+    "
+    Write-Host "Info: Run sql command: $sql"
+    $dataset = new-object "System.Data.Dataset"
+    $dataadapter = new-object "System.Data.SqlClient.SqlDataAdapter" ($sql, $connection)
+    $null = $dataadapter.Fill($dataset)
+
+    foreach ($row in $dataset.Tables.rows) {
+        $image = $row.ARMImage
+        Invoke-SmokeTest -ARMImage $image -Location $Location
     }
-    if ($command) {
-        $command.Dispose();
-    }
+
+    $dataadapter.Dispose()
+    $dataset.Dispose()
+}
+finally {
     if ($connection) {
         $connection.Close()
         $connection.Dispose()
     }
-} else {
-    Write-Host "Error: Database details are not provided."
 }
+
