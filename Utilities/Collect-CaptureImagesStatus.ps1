e64ca3e60c8ab45ab6355655bd1242d4cb025c87
Baihua Lu <balu@microsoft.com>
2020-11-16 18:55:33 +0800

    Add Collect smoke test code

---
+++ Utilities/Collect-CaptureImagesStatus.ps1
@@ -0,0 +1,207 @@
+##############################################################################################
+# Copyright (c) Microsoft Corporation
+# Licensed under the Apache License.
+#
+# Collect-CaptureImagesStatus.ps1
+###############################################################################################
+Param
+(
+    [String] $AzureSecretsFile,
+    [String] $TestPass,
+    [String] $DbServer,
+    [String] $DbName,
+    [String] $Title
+)
+
+#Load libraries
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$commonModulePath = Join-Path $scriptPath "CommonFunctions.psm1"
+Import-Module $commonModulePath
+
+$StatusNotStarted = "NotStarted"
+$StatusRunning = "Running"
+$StatusPassed = "Passed"
+$StatusFailed = "Failed"
+
+function Get-PassedCount($connection) {
+    $sql = "
+    select Count(ARMImage) from CaptureImageInfo
+    where Status='$StatusPassed'"
+
+    $results = QuerySql $connection $sql
+    $count = ($results[0][0]) -as [int]
+    Write-LogInfo "The passed count is $count"
+
+    return $count
+}
+
+function Get-FailedCount($connection) {
+    $sql = "
+    select Count(ARMImage) from CaptureImageInfo
+    where Status='$StatusFailed'"
+
+    $results = QuerySql $connection $sql
+    $count = ($results[0][0]) -as [int]
+    Write-LogInfo "The failed count is $count"
+
+    return $count
+}
+
+function Get-TotalCount($connection) {
+    $sql = "
+    select Count(ARMImage) from CaptureImageInfo"
+
+    $results = QuerySql $connection $sql
+    $count = ($results[0][0]) -as [int]
+    Write-LogInfo "The total count is $count"
+
+    return $count
+}
+
+function Get-RunningCount($connection) {
+    $sql = "
+    select Count(ARMImage) from CaptureImageInfo
+    where Status='$StatusRunning'"
+
+    $results = QuerySql $connection $sql
+    $count = ($results[0][0]) -as [int]
+    Write-LogInfo "The running count is $count"
+
+    return $count
+}
+
+function Get-NotStartedCount($connection) {
+    $sql = "
+    select Count(ARMImage) from CaptureImageInfo
+    where Status='$StatusNotStarted'"
+
+    $results = QuerySql $connection $sql
+    $count = ($results[0][0]) -as [int]
+    Write-LogInfo "The not started count is $count"
+
+    return $count
+}
+
+# Read secrets file and terminate if not present
+Write-LogInfo "Check the Azure Secrets File..."
+if (![String]::IsNullOrEmpty($AzureSecretsFile) -and (Test-Path -Path $AzureSecretsFile)) {
+    $content = Get-Content -Path $AzureSecretsFile
+    foreach ($line in $content) {
+        if ($line.split(':')[0] -eq 'dbUserName') {
+            $dbuser = $line.split(':')[1].trim()
+        }
+        if ($line.split(':')[0] -eq 'dbPassword') {
+            $dbpassword = $line.split(':')[1].trim()
+        }
+    }
+} else {
+    Write-LogErr "Please provide value for -AzureSecretsFile"
+    exit 1
+}
+
+$server = $DbServer
+$database = $DbName
+if (!$server -or !$dbuser -or !$dbpassword -or !$database) {
+    Write-LogErr "Database details are not provided."
+    exit 1
+}
+
+$connectionString = "Server=$server;uid=$dbuser;pwd=$dbpassword;Database=$database;" +
+                    "Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;MultipleActiveResultSets=True;"
+
+try {
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = $connectionString
+    $connection.Open()
+
+    $totalCount = Get-TotalCount $connection
+    $notStartedCount = Get-NotStartedCount $connection
+    $runningCount = Get-RunningCount $connection
+    $passedCount = Get-PassedCount $connection
+    $failedCount = Get-FailedCount $connection
+} catch {
+    $line = $_.InvocationInfo.ScriptLineNumber
+    $script_name = ($_.InvocationInfo.ScriptName).Replace($PWD,".")
+    $ErrorMessage =  $_.Exception.Message
+    Write-LogErr "EXCEPTION : $ErrorMessage"
+    Write-LogErr "Source : Line $line in script $script_name."
+    exit 1
+} 
+finally {
+    if ($null -ne $connection) {
+        $connection.Close()
+        $connection.Dispose()
+    }
+}
+
+# region HTML File structure
+$TableStyle = '
+<style type="text/css">
+  .tm  {border-collapse:collapse;border-spacing:0;border-color:#999;}
+  .tm td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+  .tm th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}
+  .tm .tm-dk6e{font-weight:bold;color:#ffffff;text-align:center;vertical-align:top}
+  .tm .tm-xa7z{background-color:#ffccc9;vertical-align:top}
+  .tm .tm-ys9u{background-color:#b3ffd9;vertical-align:top}
+  .tm .tm-7k3a{background-color:#D2E4FC;font-weight:bold;text-align:center;vertical-align:top}
+  .tm .tm-yw4l{vertical-align:top}
+  .tm .tm-6k2t{background-color:#D2E4FC;vertical-align:top}
+</style>
+'
+
+$htmlHeader = '
+<h2>&bull;&nbsp;STATUS_TITLE</h2>
+<table border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse" class="tm">
+  <tr>
+    <th class="tm-dk6e" colspan="9">Azure Fleet Images Capture Status</th>
+  </tr>
+  <tr>
+    <td class="tm-7k3a">Total Images Count</td>
+    <td class="tm-7k3a">Not Started</td>
+    <td class="tm-7k3a">Running</td>
+    <td class="tm-7k3a">Passed</td>
+    <td class="tm-7k3a">Failed</td>
+  </tr>
+'
+
+$htmlNodeRed =
+'
+  <tr>
+    <td class="tm-yw4l">TOTAL</td>
+    <td class="tm-yw4l">NOTSTARTED</td>
+    <td class="tm-yw4l">RUNNING</td>
+    <td class="tm-yw4l">PASSED</td>
+    <td class="tm-yw4l">FAILED</td>
+  </tr>
+'
+
+$htmlEnd =
+'
+</table>
+'
+#endregion
+$ReportHTMLFile = "AzureFleetSmokeTestStatus.html"
+if (!(Test-Path -Path ".\AzureFleetSmokeTestStatus.html")) {
+    $htmlHeader = $TableStyle + $htmlHeader
+}
+
+#region Get Title...
+
+$htmlHeader = $htmlHeader.Replace("STATUS_TITLE","$Title")
+#endregion
+
+#region build HTML Page
+$finalHTMLString = $htmlHeader
+
+$currentNode = $htmlNodeRed
+$currentNode = $currentNode.Replace("TOTAL","$totalCount")
+$currentNode = $currentNode.Replace("NOTSTARTED","$notStartedCount")
+$currentNode = $currentNode.Replace("RUNNING","$runningCount")
+$currentNode = $currentNode.Replace("PASSED","$passedCount")
+$currentNode = $currentNode.Replace("FAILED","$failedCount")
+$finalHTMLString += $currentNode
+$finalHTMLString += $htmlEnd
+
+Add-Content -Value $finalHTMLString -Path $ReportHTMLFile
+Write-Host "Azure Fleet Smoke Test report is ready."
+#endregion
